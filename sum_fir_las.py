45045703e14b6f6f1a77608e564e51939ae7f416
swethacmd2002 <58969272+swethacmd2002@users.noreply.github.com>
2019-12-31 00:32:24 +0530

    Update sum_fir_las.py

--- sum_fir_las.py
+++ sum_fir_las.py
@@ -1,9 +1,14 @@
+# program to find first and last digits of a number 
+  
+# Find the first digit 
 def firstDigit(n) : 
-    while n >= 10:  
+  while n >= 10:  
         n = n / 10; 
-    return int(n) 
+  return int(n) 
+  
+# Find the last digit 
 def lastDigit(n) : 
-    return (n % 10) 
-n = 98562; 
+  return (n % 10) 
+n = int(input())
 print(firstDigit(n), end = " ")  
-print(lastDigit(n)) 
+print(lastDigit(n))
