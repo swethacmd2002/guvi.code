6d7900eed6341fbf6d836a2b17902207ee2f750b
swethacmd2002 <58969272+swethacmd2002@users.noreply.github.com>
2019-12-30 23:45:26 +0530

    Delete largest_3numbers.py

--- largest_3numbers.py
+++
@@ -1,9 +0,0 @@
-a = float(input(" Please Enter the First Value a: "))
-b = float(input(" Please Enter the Second Value b: "))
-c = float(input(" Please Enter the Third Value c: "))
-if(a > b > c):
-    print("a is Greater than b")
-elif(b > a > c):
-    print("b is Greater than a")
-else:
-    print("c is Greater")
