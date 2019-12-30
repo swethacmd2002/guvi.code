1543934f8e3c5d9510e1d28e1cf53fdc9bdc6018
swethacmd2002 <58969272+swethacmd2002@users.noreply.github.com>
2019-12-31 00:42:24 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,12 @@
-print("Register the account: ")
-user=input("Enter the username: ")
-password=int(input("Enter the password: "))
-correctpassword=int(input("password"))
-if password=correct:
- print("Loggedin")
+#palindrome or not
+n=int(input("Enter number:"))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
+    print("The number is a palindrome!")
+else:
+    print("The number isn't a palindrome!")
