21fe33e725cf8eb241f46f30316ee531d5cdf30e
swethacmd2002 <58969272+swethacmd2002@users.noreply.github.com>
2020-06-07 02:28:13 +0530

    Add files via upload

---
+++ QUIZ/dist/js/timer.js
@@ -0,0 +1,19 @@
+let dt = new Date(new Date().setTime(0));
+let time = dt.getTime();
+let seconds = Math.floor((time % (100 * 60))/ 1000);
+let minutes = Math.floor((time % (100 * 60 * 60))/( 1000 * 60));
+let timex = 0;
+
+let mytime=setInterval(function(){
+        
+    if(seconds < 59) {
+        seconds++;
+    } else {
+            
+        minutes++;
+        seconds = 0;
+    }
+    let formatted_sec = seconds < 10 ? `0${seconds}`: `${seconds}`;
+    let formatted_min = minutes < 10 ? `0${minutes}`: `${minutes}`
+    document.querySelector("span.time").innerHTML = `${formatted_min} : ${formatted_sec}`;
+}, 1000);
